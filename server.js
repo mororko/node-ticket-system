7a90313ed5951356ae96249cefdf7e64357f85e3
Diego <mora8mola@gmail.com>
2025-04-06 20:37:35 +0200

    upd

---
+++ server.js
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import app from "./app.js";
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+export default server;
